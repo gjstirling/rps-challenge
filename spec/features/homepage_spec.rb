7fbf51cbb0142c718a172ee9fa32e13d5ddbbe15
Graeme Stirling <graemejstirling@gmail.com>
2021-08-18 11:23:36 +0100

    Corrected rspec test (missing visit command)

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,7 +1,9 @@
 require 'spec_helper'
+require './app'
 
 feature 'loading home page' do
   scenario 'display welcome string' do
+    visit '/'
     expect(page).to have_content 'Hello World!'
   end
 end
